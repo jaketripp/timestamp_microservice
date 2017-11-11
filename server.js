bf810b4445158b47a28674f2da4edb3376ef0df9
Jake Tripp <shmogens@gmail.com>
2017-11-11 13:05:55 -0600

    get server working, add index.html file

--- server.js
+++ server.js
@@ -1,20 +1,43 @@
 const express = require('express');
 const moment = require('moment-timezone');
 
-app.get('/:time', (req, res) => {
+const port = process.env.PORT || 3000;
+var app = express();
 
+app.use(express.static(__dirname));
+
+app.get('/', (req, res) => {
+	res.render('index.html');
 });
 
-function checkTimeStamp(time) {
-	if (typeof time === 'number') {
-		return {
-			"unix": time,
-			"natural": 
+app.get('/:time', (req, res) => {
+	var time = req.params.time;
+	res.send(checkAndReturnTimeStamp(time));
+});
+
+app.listen(port, () => {
+	console.log(`Server is up on port ${port}`);
+});
+
+// { "unix": 1450159200000, "natural": "December 15, 2015" }
+
+function checkAndReturnTimeStamp(time) {
+	// includes a letter
+	if (isNaN(time)) {
+		if (new Date(time).toString() === 'Invalid Date') {
+			return new Timestamp(null, null);
 		}
-	} else if (typeof time === 'string') {
-		return {
-			"unix": ,
-			"natural": 
-		}
+		var unix = new Date(time).getTime();
+		return new Timestamp(unix, time);
+	// only numbers
+	} else {
+		var uglyDate = new Date(Number(time));
+		var natural = moment(uglyDate).tz("America/Monterrey").format("MMMM DD, YYYY");
+		return new Timestamp(time, natural);
 	}
+}
+
+function Timestamp(unix, natural) {
+    this.unix = unix;
+    this.natural = natural;
 }
